a45df998612a650dd0d00cef315e8c40c520289a
Isaque Pereira dos Santos <isaquevidaltube@gmail.com>
2025-08-22 20:37:05 -0300

    code of task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,51 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
+//import { use } from 'chai';
+//import { todo } from 'node:test';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+
+import users from '../public/api/users.json';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  const handleShowTodo = (todo: Todo) => {
+    const user = users.find(u => u.id === todo.userId);
+
+    setSelectedTodo({ ...todo, user });
+    setIsModalOpen(true);
+  };
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        setLoading(true);
+        const todosFromApi = await getTodos();
+
+        setTodos(todosFromApi);
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error('Error fetching todos:', error);
+        // eslint-disable-next-line no-console
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTodos();
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -21,14 +58,21 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && <Loader />}
+
+              {!loading && todos.length > 0 && (
+                <TodoList todos={todos} onShow={handleShowTodo} />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      <TodoModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        todo={selectedTodo}
+      />
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  todo: Todo | null;
+  loading?: boolean;
+};
+
+export const TodoModal: React.FC<Props> = ({
+  isOpen,
+  onClose,
+  todo,
+  loading = false,
+}) => {
+  if (!isOpen) {
+    return null;
+  }
+
   return (
-    <div className="modal is-active" data-cy="modal">
-      <div className="modal-background" />
+    <div className={`modal ${isOpen ? 'is-active' : ''}`} data-cy="modal">
+      <div className="modal-background" onClick={onClose} />
 
-      {true ? (
+      {loading || !todo ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,25 +32,26 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              Todo #{todo.id}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={onClose}
+            />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {todo.title}
             </p>
 
             <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
-              {' by '}
-
-              <a href="mailto:Sincere@april.biz">Leanne Graham</a>
+              <strong className="has-text-danger">Planned</strong> by{' '}
+              <a href={`mailto:${todo.user?.email}`}>{todo.user?.name}</a>
             </p>
           </div>
         </div>
